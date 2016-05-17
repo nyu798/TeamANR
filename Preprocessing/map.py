ab2792532eb8491491369de1ab5d27d9bbfe1764
nyu798 <navneet08.jain@gmail.com>
2016-05-16 21:16:44 -0400

    Preprocessing map Reduce Programs Update

--- Preprocessing/map.py
+++ Preprocessing/map.py
@@ -3,90 +3,103 @@
 import sys
 import json
 import datetime
-import numpy
-import matplotlib
-import urllib2
 from rtree import index as rtree
-from matplotlib.path import Path
 from shapely.geometry import Point, shape
+import matplotlib
+from matplotlib.path import Path
+import numpy
+import urllib2
 
 sys.path.append('.')
 
 # Filtering by checking the validity of the location
 def checkLocationValidity(location):
-    try:
-        if(location[0] != 0 and location[1] != 0):
-            return True
-        else:
-            return False
-    except:
-        print >> sys.stderr, ("Location Invalid", location)
-        return False
+	try:
+		if(location[0] != 0 and location[1] != 0):
+			return True
+		else:
+			return False
+	except:
+		return False
 		
 # Check the validity of pickup and dropoff time
-def comparePickDropTime(pickup, dropoff):
-    try:
-        pickup_date, pickup_time = pickup.split(' ')
-        pickup_year, pickup_month, pickup_day = pickup_date.split('-')
-        pickup_hour, pickup_minute, pickup_sec = pickup_time.split(':')
-        
-        dropoff_date, dropoff_time = dropoff.split(' ')
-        dropoff_year, dropoff_month, dropoff_day = dropoff_date.split('-')
-        dropoff_hour, dropoff_minute, dropoff_sec = dropoff_time.split(':')
+def comparePickDropTime(pickup_value, dropoff_value):
+	try:
+		pickup_date, pickup_time = pickup_value.split(' ')
+		pickup_yr, pickup_mth, pickup_day = pickup_date.split('-')
+		pickup_hrs, pickup_mins, pickup_secs = pickup_time.split(':')
 		
-        return (datetime.datetime(int(pickup_year), int(pickup_month), int(pickup_day), int(pickup_hour), int(pickup_minute), int(pickup_sec))) < (datetime.datetime(int(dropoff_year), int(dropoff_month), int(dropoff_day), int(dropoff_hour), int(dropoff_minute), int(dropoff_sec)))
-    except:
-        print >> sys.stderr, (pickup, dropoff)
-        return False
+		dropoff_date, dropoff_time = dropoff_value.split(' ')
+		dropoff_yr, dropoff_mth, dropoff_day = dropoff_date.split('-')
+		dropoff_hrs, dropoff_mins, dropoff_secs = dropoff_time.split(':')
+		
+		pickup_yr = int(pickup_yr)
+		pickup_mth = int(pickup_mth)
+		pickup_day = int(pickup_day)
+		pickup_hrs = int(pickup_hrs)
+		pickup_mins = int(pickup_mins)
+		pickup_secs = int(pickup_secs)
+		dropoff_yr = int(dropoff_yr)
+		dropoff_mth = int(dropoff_mth)
+		dropoff_day = int(dropoff_day)
+		dropoff_hrs = int(dropoff_hrs)
+		dropoff_mins = int(dropoff_mins)
+		dropoff_secs = int(dropoff_secs)
+		
+		time1 = datetime.datetime(pickup_yr, pickup_mth, pickup_day, pickup_hrs, pickup_mins, pickup_secs)
+		time2 = datetime.datetime(dropoff_yr, dropoff_mth, dropoff_day, dropoff_hrs, dropoff_mins, dropoff_secs)
+		
+		return time1 < time2
+	except:
+		return False
 
-def readNeighborhoodData(shape_file, index, neighborhoods):
-    for entry in shape_file:
-        paths = map(Path, entry['geometry']['coordinates'])
-        bbox = paths[0].get_extents()
-        map(bbox.update_from_path, paths[1:])
-        index.insert(len(neighborhoods), list(bbox.get_points()[0])+list(bbox.get_points()[1]))
-        neighborhoods.append((entry['properties']['borough'], entry['properties']['neighborhood'], shape(entry['geometry'])))
-    neighborhoods.append(('UNKNOWN', 'UNKNOWN', None))    
-
-def locateNeighborhood(location, index, neighborhoods):
-    try:
-        if(checkLocationValidity(location)):
-            point = Point(location[0], location[1])
-            match = index.intersection((location[0], location[1], location[0], location[1]))
-            for entry in match:
-                if neighborhoods[entry][2].contains(point):
-                    return entry
-        return -1
-    except Exception,e:
-        print >> sys.stderr, ("Neighborhood Invalid", location, str(e))
-        return -1
-        
+def locateNeighborhood(index, location, areas):
+	try:
+		if(checkLocationValidity(location)):
+			point = Point(location[0], location[1])
+			match = index.intersection((location[0], location[1], location[0], location[1]))
+			for entry in match:
+				if areas[entry][2].contains(point):
+					return entry
+		return -1
+	except Exception,e:
+		return -1
+		
 def readInput():
-    for line in sys.stdin:
-        line = line.strip('\n')
-        values = line.split(',')
-        if len(values) > 1 and values[0] != 'VendorID': 
-            yield values
+	for line in sys.stdin:
+		line = line.strip()
+		values = line.split(',')
+		if len(values) > 1 and values[0] != 'VendorID': 
+			yield values
 
 def mapper():
-    # Import file as JSON
-    with open('pediacitiesnycneighborhoods.geojson') as f:
+	# Import file as JSON
+	with open('pediacitiesnycneighborhoods.geojson') as f:
 		js = json.load(f)
 	
-    neighborhoods = []
-    index = rtree.Index()    
-    readNeighborhoodData(js['features'], index, neighborhoods)
-    
-    for values in readInput():
-        if(comparePickDropTime(values[1], values[2])):
-            pickup_location = (float(values[5]), float(values[6]))
-            dropoff_location = (float(values[9]), float(values[10]))
-            
-            pickup_index = locateNeighborhood(pickup_location, index, neighborhoods)
-            dropoff_index = locateNeighborhood(dropoff_location, index, neighborhoods)
-            
-            print "%s\t%s,%s,%s,%s" % (','.join(values), neighborhoods[pickup_index][0], neighborhoods[pickup_index][1], neighborhoods[dropoff_index][0], neighborhoods[dropoff_index][1])
+	areas = []
+	
+	index = rtree.Index()
+	
+	for entry in js['features']:
+		paths = map(Path, entry['geometry']['coordinates'])
+		bbox = paths[0].get_extents()
+		map(bbox.update_from_path, paths[1:])
+		size = len(areas)
+		index.insert(size, list(bbox.get_points()[0])+list(bbox.get_points()[1]))
+		areas.append((entry['properties']['borough'], entry['properties']['neighborhood'], shape(entry['geometry'])))
+	areas.append(('UNKNOWN', 'UNKNOWN', None))
+	
+	for values in readInput():
+		if(comparePickDropTime(values[1], values[2])):
+			pickup_location = (float(values[5]), float(values[6]))
+			dropoff_location = (float(values[9]), float(values[10]))
+			
+			p_index = locateNeighborhood(index, pickup_location, areas)
+			d_index = locateNeighborhood(index, dropoff_location, areas)
+			
+			print "%s\t%s,%s,%s,%s" % (','.join(values), areas[p_index][0], areas[p_index][1], areas[d_index][0], areas[d_index][1])
 
 
 if __name__=='__main__':
-    mapper()
+	mapper()
--- Preprocessing/reduce.py
+++ Preprocessing/reduce.py
@@ -3,12 +3,13 @@
 import sys
 
 def readInput():
-    for line in sys.stdin:
-        yield line.strip('\n').split('\t')
+	for line in sys.stdin:
+		line = line.strip()
+		yield line.split('\t')
 
 def reducer():
-    for key, value in readInput():
-        print '%s,%s' % (key, value)
+	for key, value in readInput():
+		print '%s,%s' % (key, value)
 
 if __name__=='__main__':
-    reducer()
+	reducer()
